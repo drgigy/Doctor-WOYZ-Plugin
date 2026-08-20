2c4e03de6ec07926c664b45a8ff78622d85009cf
Gigy Kuruttukulam <gigy@Gigys-MacBook-Air.local>
2026-08-20 12:50:50 +0530

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyDaSqQLixjaib7hfsv9hsLzebWU_kZuLmY",
   authDomain: "woyz-be9e5.firebaseapp.com",
   projectId: "woyz-be9e5",
-  appId: ""
+  storageBucket: "woyz-be9e5.firebasestorage.app",
+  messagingSenderId: "757935575451",
+  appId: "1:757935575451:web:4adba5f60e49346cce513e",
+  measurementId: "G-LBPXGJCKRB"
 };
